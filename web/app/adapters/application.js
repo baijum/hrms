f1f6216ef8e3b76539a475827dcca1d3d12c79fa
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-06-10 11:04:12 +0530

    simple auth mixin for data adapter

---
+++ web/app/adapters/application.js
@@ -0,0 +1,6 @@
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:application'
+});
