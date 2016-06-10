c6c523046cb0f806477583b1a7593685954b8ebe
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-06-10 16:16:23 +0530

    minimal authentication

--- hrms.go
+++ hrms.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/codegangsta/negroni"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
 
-var web = flag.String("web", "web/dist", "Web UI directory")
+var (
+	web        = flag.String("web", "web/dist", "Web UI directory")
+	signingKey = []byte("yu786lklgfso32921lkasaskdhladsyg6")
+)
 
 func TokenAuth(w http.ResponseWriter, r *http.Request) {
-	// TODO: Complete this function
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Printf("%#v\n", r)
+	token := jwt.New(jwt.GetSigningMethod("HS256"))
+	// FIXME: get the username from the request
+	token.Claims["sub"] = "mbaiju"
+	token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+	// Sign and get the complete encoded token as a string
+	tokenString, _ := token.SignedString(signingKey)
+
+	s := map[string]string{"token": tokenString}
+
+	out, _ := json.Marshal(s)
+	w.Write(out)
 }
 
 func main() {
