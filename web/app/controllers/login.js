c6b6c6ad3cccdb25474839571bfc02aeeae7ed23
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-06-10 10:43:58 +0530

    remove unused code

--- web/app/controllers/login.js
+++ web/app/controllers/login.js
@@ -5,10 +5,7 @@ export default Ember.Controller.extend({
 
   actions: {
     authenticate() {
-      var credentials = this.getProperties('identification', 'password');
-      console.log(credentials);
       let { identification, password } = this.getProperties('identification', 'password');
-      console.log(identification, password);
       this.get('session').authenticate('authenticator:jwt', identification, password).catch((reason) => {
         this.set('errorMessage', reason.error || reason);
       });
