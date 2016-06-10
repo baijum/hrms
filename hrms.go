fe4ae1395a5962f3f6c37741d61726e20c9fff0b
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-06-10 11:10:27 +0530

    request handler for /api/token-auth/

--- hrms.go
+++ hrms.go
@@ -10,12 +10,18 @@ import (
 
 var web = flag.String("web", "web/dist", "Web UI directory")
 
+func TokenAuth(w http.ResponseWriter, r *http.Request) {
+	// TODO: Complete this function
+}
+
 func main() {
 	flag.Parse()
 	r := mux.NewRouter()
+	r.HandleFunc("/api/token-auth/", TokenAuth).Methods("POST")
 	n := negroni.New(negroni.NewRecovery(),
 		negroni.NewLogger(),
 		negroni.NewStatic(http.Dir(*web)))
 	n.UseHandler(r)
 	n.Run(":7080")
+
 }
