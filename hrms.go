4180b1250d9fa3d94961089056662083cf638ddb
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-06-13 17:03:54 +0530

    check user credentials

--- hrms.go
+++ hrms.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -15,23 +14,39 @@ import (
 var (
 	web        = flag.String("web", "web/dist", "Web UI directory")
 	signingKey = []byte("yu786lklgfso32921lkasaskdhladsyg6")
+	users      = map[string]string{
+		"mbaiju": "qwerty123",
+		"somkar": "asdfgh123",
+	}
 )
 
 func TokenAuth(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Printf("%#v\n", r)
-	token := jwt.New(jwt.GetSigningMethod("HS256"))
-	// FIXME: get the username from the request
-	token.Claims["sub"] = "mbaiju"
-	token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-	// Sign and get the complete encoded token as a string
-	tokenString, _ := token.SignedString(signingKey)
+	var pl map[string]string
 
-	s := map[string]string{"token": tokenString}
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&pl)
+	username := pl["username"]
+	password := pl["password"]
 
-	out, _ := json.Marshal(s)
-	w.Write(out)
+	if passwd, found := users[username]; found {
+		if password == passwd {
+			token := jwt.New(jwt.GetSigningMethod("HS256"))
+			// FIXME: get the username from the request
+			token.Claims["sub"] = username
+			token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+			// Sign and get the complete encoded token as a string
+			tokenString, _ := token.SignedString(signingKey)
+
+			s := map[string]string{"token": tokenString}
+
+			out, _ := json.Marshal(s)
+			w.Write(out)
+			w.WriteHeader(http.StatusOK)
+		}
+	}
+	w.WriteHeader(http.StatusUnauthorized)
+
 }
 
 func main() {
